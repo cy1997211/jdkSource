412d2e1912de8bdc9faddb0994be823f195a1723
guynemoww <guynemo@163.com>
2019-02-25 13:48:34 +0800

    增加url调用的方法

---
+++ src/main/java/jdk/rt/java/util/common/UrlTest1.java
@@ -0,0 +1,27 @@
+package jdk.rt.java.util.common;
+
+import cn.hutool.http.HttpUtil;
+import cn.hutool.setting.Setting;
+
+/**
+ * @author teamsun_wangwei
+ * @version V1.0.0
+ * @Title:
+ * @Package jdk.rt.java.util.common
+ * @date 2019/2/25 10:17
+ * @Description:
+ */
+public class UrlTest1 {
+
+    public static void main(String[] args) {
+        Setting setting = new Setting("url.properties");
+        String url = setting.get("sina.url");
+        String content = HttpUtil.get(url);
+
+        System.out.println(content);
+
+
+    }
+
+
+}
